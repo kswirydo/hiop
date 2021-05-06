2316320f120984b11a73803fee62bcaee036532c
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2021-05-06 09:31:37 -0700

    Update newell environment

--- scripts/newellVariables.sh
+++ scripts/newellVariables.sh
@@ -3,7 +3,6 @@ if [ ! -v BUILDDIR ]; then
 fi
 export PROJ_DIR=/qfs/projects/exasgd
 export APPS_DIR=/share/apps
-export SPACK_ARCH=linux-rhel7-power9le
 #  NOTE: The following is required when running from Gitlab CI via slurm job
 source /etc/profile.d/modules.sh
 module use -a /usr/share/Modules/modulefiles
@@ -11,25 +10,19 @@ module use -a /share/apps/modules/tools
 module use -a /share/apps/modules/compilers
 module use -a /share/apps/modules/mpi
 module use -a /etc/modulefiles
-module use -a $PROJ_DIR/src/spack/share/spack/modules/$SPACK_ARCH/
-export MY_GCC_VERSION=7.4.0
-export MY_CUDA_VERSION=10.2
-export MY_OPENMPI_VERSION=3.1.5
-export MY_CMAKE_VERSION=3.16.4
-export MY_MAGMA_VERSION=2.5.4-gcc-7.4.0-jss5aen
-export MY_METIS_VERSION=5.1.0
+source $PROJ_DIR/src/spack/share/spack/setup-env.sh
 export MY_NVCC_ARCH="sm_70"
 
-module load gcc/$MY_GCC_VERSION
-module load cuda/$MY_CUDA_VERSION
-module load openmpi/$MY_OPENMPI_VERSION
-module load cmake/$MY_CMAKE_VERSION
-module load magma/$MY_MAGMA_VERSION
-
-export NVBLAS_CONFIG_FILE=$PROJ_DIR/$MY_CLUSTER/nvblas.conf
-export MY_RAJA_DIR=$PROJ_DIR/$MY_CLUSTER/raja
-export MY_UMPIRE_DIR=$PROJ_DIR/$MY_CLUSTER/umpire
-export MY_UMFPACK_DIR=$PROJ_DIR/$MY_CLUSTER/suitesparse
-export MY_METIS_DIR=$APPS_DIR/metis/$MY_METIS_VERSION
-export MY_HIOP_MAGMA_DIR=$APPS_DIR/magma/2.5.2/cuda10.2
-export MY_COINHSL_DIR=$PROJ_DIR/$MY_CLUSTER/ipopt
+module load gcc/7.4.0
+module load cuda/10.2
+module load openmpi/3.1.5
+module load cmake/3.19.6
+spack env activate hiop-v0-4-2-deps-newell
+cat >>$BUILDDIR/nvblas.conf <<EOD
+NVBLAS_LOGFILE  nvblas.log
+NVBLAS_CPU_BLAS_LIB /qfs/projects/exasgd/src/spack/var/spack/environments/hiop-v0-4-2-deps-newell/.spack-env/view/lib/libopenblas.so
+NVBLAS_GPU_LIST ALL
+NVBLAS_TILE_DIM 2048
+NVBLAS_AUTOPIN_MEM_ENABLED
+EOD
+export NVBLAS_CONFIG_FILE=$BUILDDIR/nvblas.conf
