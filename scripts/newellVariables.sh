53226621af158a13f290d856e7982c0b8d245162
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2021-05-12 09:23:00 -0700

    Use new env for newell

--- scripts/newellVariables.sh
+++ scripts/newellVariables.sh
@@ -12,23 +12,23 @@ module use -a /share/apps/modules/compilers
 module use -a /share/apps/modules/mpi
 module use -a /etc/modulefiles
 module use -a $PROJ_DIR/src/spack/share/spack/modules/$SPACK_ARCH/
-export MY_GCC_VERSION=7.4.0
-export MY_CUDA_VERSION=10.2
-export MY_OPENMPI_VERSION=3.1.5
-export MY_CMAKE_VERSION=3.16.4
-export MY_MAGMA_VERSION=2.5.4-gcc-7.4.0-jss5aen
-export MY_METIS_VERSION=5.1.0
-export MY_NVCC_ARCH="sm_70"
 
-module load gcc/$MY_GCC_VERSION
-module load cuda/$MY_CUDA_VERSION
-module load openmpi/$MY_OPENMPI_VERSION
-module load cmake/$MY_CMAKE_VERSION
-module load magma/$MY_MAGMA_VERSION
+module load gcc/7.4.0
+module load cuda/10.2
+module load openmpi/3.1.5
+module load cmake/3.19.6
 
-export NVBLAS_CONFIG_FILE=$PROJ_DIR/$MY_CLUSTER/nvblas.conf
-export MY_RAJA_DIR=$PROJ_DIR/$MY_CLUSTER/raja
-export MY_UMPIRE_DIR=$PROJ_DIR/$MY_CLUSTER/umpire
-export MY_UMFPACK_DIR=$PROJ_DIR/$MY_CLUSTER/suitesparse
-export MY_METIS_DIR=$APPS_DIR/metis/$MY_METIS_VERSION
-export MY_COINHSL_DIR=$PROJ_DIR/$MY_CLUSTER/ipopt
+source $PROJ_DIR/src/spack/share/spack/setup-env.sh
+spack env activate hiop-v0-4-2-deps-newell
+
+[ -f $BUILDDIR/nvblas.conf ] && rm $BUILDDIR/nvblas.conf
+cat >>$BUILDDIR/nvblas.conf <<EOD
+NVBLAS_LOGFILE  nvblas.log
+NVBLAS_TRACE_LOG_ENABLED
+NVBLAS_CPU_BLAS_LIB `spack location -i openblas`
+NVBLAS_GPU_LIST ALL0
+NVBLAS_TILE_DIM 2048
+NVBLAS_AUTOPIN_MEM_ENABLED
+EOD
+export NVBLAS_CONFIG_FILE=$BUILDDIR/nvblas.conf
+export EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DCMAKE_CUDA_ARCHITECTURES=60"
